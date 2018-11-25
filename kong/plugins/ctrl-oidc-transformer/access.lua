f79c6f71ce0cf7a676a640cd7fd3ea7b29b98dc1
Parthasarathy B <bakparthasarathy@deloitte.nl>
2018-11-25 18:27:53 +0100

    update array lookup

--- kong/plugins/ctrl-oidc-transformer/access.lua
+++ kong/plugins/ctrl-oidc-transformer/access.lua
@@ -10,7 +10,7 @@ function _M.execute(conf)
     ngx.log(ngx.INFO, "found "..conf.input_header_name.." header and transforming...")
     local decoded_text = ngx.decode_base64(req_get_headers()[conf.input_header_name])
     local userObj = cjson.decode(decoded_text)
-    ngx_set_header(conf.output_header_name, userObj.username)
+    ngx_set_header(conf.output_header_name, userObj["username"])
   else
     ngx.log(ngx.ERR, conf.input_header_name.." header unavaiable")
   end
