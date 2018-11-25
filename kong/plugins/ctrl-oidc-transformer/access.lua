18bd9cd0d38484581c4abbcd40e965190c48db29
Parthasarathy B <bakparthasarathy@deloitte.nl>
2018-11-25 18:09:33 +0100

    remove semicolon

--- kong/plugins/ctrl-oidc-transformer/access.lua
+++ kong/plugins/ctrl-oidc-transformer/access.lua
@@ -8,8 +8,8 @@ function _M.execute(conf)
   
   if req_get_headers()[conf.input_header_name] then
     ngx.log(ngx.INFO, "found "..conf.input_header_name.." header and transforming...")
-    local decoded_text = ngx.decode_base64(req_get_headers()[conf.input_header_name]);
-    local userObj = cjson.decode(decoded_text);
+    local decoded_text = ngx.decode_base64(req_get_headers()[conf.input_header_name])
+    local userObj = cjson.decode(decoded_text)
     ngx_set_header(conf.output_header_name, userObj.username)
   else
     ngx.log(ngx.ERR, conf.input_header_name.." header unavaiable")
